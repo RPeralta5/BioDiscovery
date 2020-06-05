a0dcf3c0972b854ee14dc11f2eeeb7f761df523e
RPeralta5 <34075172+RPeralta5@users.noreply.github.com>
2020-06-04 20:32:15 -0700

    Added comments
    added comments and cleaned the code.
    I still need to implement the 2nd option for searching multiple chromosomes.

--- Menu.java
+++ Menu.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 
 /*
-    Public Class: Chromosome
+    Public Class: Menu
     Author: Ryan Peralta
-    Last Edit: 6/3/2020
-        Created the options and header methods.
+    Last Edit: 6/4/2020
+        Created the searchMultipleC method.
+
+     Menu class that displays the available options to the user.
+     Option 1(searchOneC) is for a query spanning 1 chromosome.
+     Option 2(searchMultipleC) is for a query spanning multiple chromosomes.
+     Option 3 is to exit the program.
+
  */
 public class Menu {
-    Scanner s = new Scanner(System.in);
+    //Scanner used to receive the choice and query input by the user
+    private Scanner s = new Scanner(System.in);
 
+    /*
+        Empty Constructor
+     */
     public Menu(){
 
     }
 
-
+    /*
+        Header method that displays the menu to the user.
+        This method waits for the user to input their choice and will check that the inputted value is a valid option.
+     */
     public void header(){
         System.out.println("      BioDiscovery Inc. Genomic Data Search      ");
         System.out.println("-------------------------------------------------");
@@ -21,7 +34,7 @@ public class Menu {
         System.out.println("2.) Search Spanning Multiple Chromosomes");
         System.out.println("3.) Exit");
 
-        //default value before try catch
+        //default value before the try catch
         int choice = 0;
         try{
             choice = s.nextInt();
@@ -30,12 +43,12 @@ public class Menu {
             System.exit(1);
         }
 
-
+        // If statements that transfer control of the program to the proper method based on the users input.
         if(choice == 1){
-            option1();
+            searchOneC();
         }
         else if(choice == 2){
-            option2();
+            searchMultipleC();
         }
         else if(choice == 3){
             System.out.println("Goodbye.");
@@ -46,8 +59,13 @@ public class Menu {
         }
     }
 
-    public void option1(){
-        //Clearing the scanner
+    /*
+        This method prompts the user for searching one chromosome.
+        It checks to make sure that the inputted query contains a : and a -.
+        It has some input validation but it can be sanitized more.
+     */
+    public void searchOneC(){
+        //Clearing the scanner -> removes the /n character from when we read the last int for the user's choice.
         s.nextLine();
 
 
@@ -56,22 +74,33 @@ public class Menu {
         System.out.println("Please insert the query in this format: chr_:start-end");
         System.out.println("Example: chr22:20980876-20980878");
         String query = s.nextLine();
+
+
         //Check that the query contains a semicolon and a dash(-)
-
-
-
-
         int indexColon = query.indexOf(":");
         int indexDash = query.indexOf("-");
         if(indexColon == -1 || indexDash == -1){
             System.out.println("Please enter the query in the correct format.");
-            option1();
+            searchOneC();
             return;
         }
+
+
+
+
+        //If the query has passed the checks, we can pass the query to the SearchOneChromosome object and begin searching for the records in the target range.
         SearchOneChromosome search1 = new SearchOneChromosome();
+
+        //long startTime = System.nanoTime();
+
+        //The SearchOne Chromosome object will take care of the rest of the search and will print out the values returned from the search.
         search1.processQuery(query);
         search1.readFile();
         search1.printValues();
+
+        //long endTime   = System.nanoTime();
+        //long totalTime = endTime - startTime;
+        //System.out.println(totalTime/1000000);
 
 
 
@@ -82,7 +111,7 @@ public class Menu {
 
 
 
-    public void option2(){
+    public void searchMultipleC(){
 
     }
 }
