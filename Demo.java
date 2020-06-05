92b30cb78ba6ef78ff3b09fd3eb095b619dbd3ba
RPeralta5 <34075172+RPeralta5@users.noreply.github.com>
2020-06-04 20:44:32 -0700

    comments - Demo

--- Demo.java
+++ Demo.java
@@ -1,8 +1,19 @@
-import java.io.*;
+/*
+    Public Class: Demo
+    Author: Ryan Peralta
+    Last Edit: 6/3/2020
+        Created an instance of menu and called the header() method.
+
+    This is the class where we will have the main method which will create an instance of Menu.
+    We will then call the header method, prompting the user to input a query starting the search.
+    Once the search finishes and the records are retrieved, they will be printed to the console.
+ */
 public class Demo {
     public static void main(String[] args) {
+
         Menu m = new Menu();
         m.header();
+
     }
 }
 
